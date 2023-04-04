f0404d2f667dbc1cf1938e428b4be514a5807b3b
Vinicius Pereira <>
2023-04-04 16:32:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,4 @@
+km_inicial = int(input('Quantos KM o seu carro percorreu?:' ))
+litros_inicial = int(input('Quantos litros você abasteceu?:' ))
+media_inicial = km_inicial / litros_inicial
+print('a média inicial de km/l é', media_inicial,'km/l')
